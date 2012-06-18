c5bc3029f7137fc248b451f79a07ded9af375f0d
Ana Rey <anarey@gmail.com>
2012-06-18 21:46:28 +0100

    fourth test: Get a long path: red

--- parseurl/test_parseurl.py
+++ parseurl/test_parseurl.py
@@ -66,3 +66,7 @@ class test_parse_path(unittest.TestCase):
     def test_parse_about(self):
         path = get_path("http://www.anarey.info/acerca-de")
         self.assertEqual(path, "acerca-de")
+
+    def test_parse_more_url(self):
+        path = get_path("http://www.anarey.info/un/paseo/por")
+        self.assertEqual(path, "un/paseo/por")
