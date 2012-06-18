8929a2b96f6ea46dd571518b84c9b1112f4cda86
Ana Rey <anarey@gmail.com>
2012-06-18 15:35:06 +0100

    Refactor the get_site()'s code

--- parseurl/parseurl.py
+++ parseurl/parseurl.py
@@ -8,12 +8,8 @@ def get_protocol(url):
     protocol = url_splitted[0]
     return protocol
 
-def get_site(site):
-    if site.endswith("www.site.com"):
-        return "www.site.com"
-    elif site.endswith("www.google.com"):
-        return "www.google.com"
-    elif site.endswith("www.twitter.com"):
-        return "www.twitter.com"
-    else:
-        return ""
+def get_site(url):
+    url_splitted = url.split("://")
+    site = url_splitted[1]
+    return site
+
