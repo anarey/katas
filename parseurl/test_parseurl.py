5e3b6208466cbc891f038d11408ac48f3281ddad
Ana Rey <anarey@gmail.com>
2012-06-18 12:10:14 +0100

    First test: green

--- parseurl/test_parseurl.py
+++ parseurl/test_parseurl.py
@@ -1,6 +1,9 @@
 #### 
 import unittest 
 
+def get_protocol(url):
+    return "http"
+
 class test_parseurl(unittest.TestCase):
     def test_protocol_http(self):
         protocol = get_protocol("http://www.site.com")
