b1db002d2fd5052f33abc2b1c65ecdbf263befde
Ana Rey <anarey@gmail.com>
2012-06-18 12:21:45 +0100

    second test. NotEqualHttp: red

--- parseurl/test_parseurl.py
+++ parseurl/test_parseurl.py
@@ -8,3 +8,12 @@ class test_parseurl(unittest.TestCase):
     def test_protocol_http(self):
         protocol = get_protocol("http://www.site.com")
         self.assertEqual("http", protocol)
+
+    def test_protocol_not_http(self):
+        protocol = get_protocol("ftp://www.site.com")
+        self.assertNotEqual("http", protocol)
+
+
+#    def test_protocol_ftp(self):
+#        protocol = get_protocol("ftp://www.site.com")
+#        self.assertEqual("http", protocol)
