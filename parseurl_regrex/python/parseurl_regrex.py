d866d6c2c4d1a722c681fad68d2f985c941f73d6
Ana Rey <anarey@gmail.com>
2012-07-02 14:48:09 +0200

    Parseurl_regrex. test_parse_regrex_protocol. Test test_not_protocol. green

--- parseurl_regrex/python/parseurl_regrex.py
+++ parseurl_regrex/python/parseurl_regrex.py
@@ -1,7 +1,14 @@
 import re
 
+class NotProtocolFound:
+    pass
+
 def get_protocol(url):
-    pattern = re.compile("https|http|ftp|smb")
-    post_pattern = pattern.search(url)
-    if post_pattern :
-        return post_pattern.group()
+
+    if re.search("://", url):
+        pattern = re.compile("https|http|ftp|smb")
+        post_pattern = pattern.search(url)
+        if post_pattern :
+            return post_pattern.group()
+    else:
+        raise NotProtocolFound
