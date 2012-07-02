21b2358811c7c4f5d8b25c8963925786e37e090e
Ana Rey <anarey@gmail.com>
2012-07-02 14:18:25 +0200

    Parseurl_regrex. test_oarse_regrex_protocol. Test test_protocol_smb. green

--- parseurl_regrex/python/parseurl_regrex.py
+++ parseurl_regrex/python/parseurl_regrex.py
@@ -3,4 +3,12 @@ import re
 def get_protocol(url):
     pattern = re.compile("http")
     post_pattern = pattern.search(url)
-    return post_pattern.group()
+    if post_pattern :
+        return post_pattern.group()
+    else:
+        pattern = re.compile("ftp")
+        post_pattern = pattern.search(url)
+        if post_pattern :
+            return "ftp"
+        else:
+            return "smb"
