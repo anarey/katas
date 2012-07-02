12b7dac43da2337a68209b271de1fe0327460047
Ana Rey <anarey@gmail.com>
2012-07-02 13:55:57 +0200

    Parseurl_regrex. test_oarse_regrex_protocol. Test test_not_protocol_http. Red

--- parseurl_regrex/python/test_parseurl_regrex.py
+++ parseurl_regrex/python/test_parseurl_regrex.py
@@ -8,10 +8,10 @@ class test_parse_regrex_protocol(unittest.TestCase):
         protocol = get_protocol("http://www.site.com")
         self.assertEqual("http", protocol)
 
-#    def test_protocol_not_http(self):
-#        protocol = get_protocol("ftp://www.site.com")
-#        self.assertNotEqual("http", protocol)
-#
+    def test_protocol_not_http(self):
+        protocol = get_protocol("ftp://www.site.com")
+        self.assertNotEqual("http", protocol)
+
 #    def test_protocol_ftp(self):
 #        protocol = get_protocol("ftp://www.site.com")
 #        self.assertEqual("ftp", protocol)
