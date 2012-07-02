712e20b5a70077dd2538cd0213b4c38af192c3e9
Ana Rey <anarey@gmail.com>
2012-07-02 14:28:22 +0200

    Refactor code parseurl_refrex.py

--- parseurl_regrex/python/parseurl_regrex.py
+++ parseurl_regrex/python/parseurl_regrex.py
@@ -1,14 +1,7 @@
 import re
 
 def get_protocol(url):
-    pattern = re.compile("https*")
+    pattern = re.compile("https|http|ftp|smb")
     post_pattern = pattern.search(url)
     if post_pattern :
         return post_pattern.group()
-    else:
-        pattern = re.compile("ftp")
-        post_pattern = pattern.search(url)
-        if post_pattern :
-            return "ftp"
-        else:
-            return "smb"
