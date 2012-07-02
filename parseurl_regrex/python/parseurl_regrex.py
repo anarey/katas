4a84c35b3240bcadd6f90475a3fcdad0a896a0b8
Ana Rey <anarey@gmail.com>
2012-07-02 18:39:09 +0200

    Refactor code. Covers all the protocols

--- parseurl_regrex/python/parseurl_regrex.py
+++ parseurl_regrex/python/parseurl_regrex.py
@@ -5,10 +5,14 @@ class NotProtocolFound:
 
 def get_protocol(url):
 
-    if re.search("://", url):
-        pattern = re.compile("https|http|ftp|smb")
-        post_pattern = pattern.search(url)
-        if post_pattern :
-            return post_pattern.group()
+    if re.search("://",url):
+        eo = re.split("://", url)
+        return eo[0]
+#        pattern = re.compile("")
+#    if re.search("://", url):
+#        pattern = re.compile("https|http|ftp|smb")
+#        post_pattern = pattern.search(url)
+#        if post_pattern :
+#            return post_pattern.group()
     else:
         raise NotProtocolFound
