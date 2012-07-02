0174f3fcddc6a5ee8e3016b1d86613e3a2cbb9e2
Ana Rey <anarey@gmail.com>
2012-07-02 14:30:03 +0200

    Parseurl_regrex. test_parse_regrex_protocol. Test test_not_protocol. Red

--- parseurl_regrex/python/test_parseurl_regrex.py
+++ parseurl_regrex/python/test_parseurl_regrex.py
@@ -20,9 +20,9 @@ class test_parse_regrex_protocol(unittest.TestCase):
         protocol = get_protocol("https://www.site.com")
         self.assertEqual("https", protocol)
 
-#    def test_not_protocol(self):
-#        self.assertRaises(NotProtocolFound, get_protocol, "www.site.com")
-#
+    def test_not_protocol(self):
+        self.assertRaises(NotProtocolFound, get_protocol, "www.site.com")
+
 #class test_parse_site(unittest.TestCase):
 #    def test_site(self):
 #        site = get_site("http://www.site.com")
