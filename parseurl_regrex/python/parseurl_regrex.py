bc96131712ab7b3124acb4a324be4cae28a10085
Ana Rey <anarey@gmail.com>
2012-07-03 01:47:43 +0200

    Parseurl_regrex. Class test_parse_regrex_site. Test test_long_url: green

--- parseurl_regrex/python/parseurl_regrex.py
+++ parseurl_regrex/python/parseurl_regrex.py
@@ -16,7 +16,9 @@ def get_site(url):
         return "www.site.com"
     elif re.search("www\.site2\.com", url):
         return "www.site2.com"
-    elif re.search("www\.google.com", url):
+    elif re.search("www\.google\.com", url):
         return "www.google.com"
-    else:
+    elif re.search("www\.twitter\.com", url):
         return "www.twitter.com"
+    else:
+        return "www.anarey.info"
