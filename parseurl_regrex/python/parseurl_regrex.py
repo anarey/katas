7722ca9b7a52664b28b540b25596762bba436f7f
Ana Rey <anarey@gmail.com>
2012-07-04 11:23:24 +0200

    refactor code in function get_protocol

--- parseurl_regrex/python/parseurl_regrex.py
+++ parseurl_regrex/python/parseurl_regrex.py
@@ -8,15 +8,17 @@ class NotSiteFound:
 
 def get_protocol(url):
 
-    if re.search("://",url):
-        eo = re.split("://", url)
-        return eo[0]
+    if re.search("://", url):
+        protocol_search = re.split("://", url)
+        protocol = protocol_search[0]
+        return protocol
     else:
         raise NotProtocolFound
 
 def get_site(url):
-    if re.search("www\..*\..*/?", url):
-        site_search = re.search("www\..*\..*/?", url)
+    pattern = re.compile("www\..*\..*/?")
+    if pattern.search(url):
+        site_search = pattern.search(url)
         site = site_search.group(0)
         site = site.split("/")[0]
     else:
