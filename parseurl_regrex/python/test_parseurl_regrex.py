186dd70095ae4a2b321cb742fdebe61ae7357157
Ana Rey <anarey@gmail.com>
2012-07-04 14:33:48 +0200

    Add three correct test

--- parseurl_regrex/python/test_parseurl_regrex.py
+++ parseurl_regrex/python/test_parseurl_regrex.py
@@ -106,27 +106,30 @@ class test_parse_url(unittest.TestCase):
         self.assertEqual(site, "www.twitter.com")
         self.assertEqual(path, "anarb/index.html")
 
-### Prueba que cumple, no falla inicialmente
-#    def test_parse_url_not_path(self):
-##        protocol, site, path = parse_url("http://www.twitter.com")
-##        self.assertEqual(protocol, "http")
-##        self.assertEqual(site, "www.twitter.com")
-##        self.assertEqual(path, "")
-#
+### Test pass in green
+    def test_parse_url_not_path(self):
+        protocol, site, path = parse_url("http://www.twitter.com")
+        self.assertEqual(protocol, "http")
+        self.assertEqual(site, "www.twitter.com")
+        self.assertEqual(path, "")
+
     def test_parse_url_not_site(self):
         protocol, site, path = parse_url("http://")
         self.assertEqual(protocol, "http")
         self.assertEqual(site, "")
         self.assertEqual(path, "")
 
-#    def test_parse_not_url(self):
-#        protocol, site, path = parse_url("ana%243f")
-#        self.assertEqual(protocol, "")
-#        self.assertEqual(site, "")
-#        self.assertEqual(path, "")
-#    def test_parse_not_url_2(self):
-#        protocol, site, path = parse_url("ssefd/index.html")
-#        self.assertEqual(protocol, "")
-#        self.assertEqual(site, "")
-#        self.assertEqual(path, "")
-#
+## Test pass in green
+    def test_parse_not_url(self):
+        protocol, site, path = parse_url("ana%243f")
+        self.assertEqual(protocol, "")
+        self.assertEqual(site, "")
+        self.assertEqual(path, "")
+
+## Test pass in green
+    def test_parse_not_url_2(self):
+        protocol, site, path = parse_url("ssefd/index.html")
+        self.assertEqual(protocol, "")
+        self.assertEqual(site, "")
+        self.assertEqual(path, "")
+
