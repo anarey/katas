5bbf937499d0a494f3ec863c5a873966492469d5
Ana Rey <anarey@gmail.com>
2012-07-06 13:35:38 +0200

    Class TestClassCalendar. Test Test_sunday_holiday. Red

--- calendar/python/test_isHoliday.py
+++ calendar/python/test_isHoliday.py
@@ -7,5 +7,9 @@ class testClassCalendar(unittest.TestCase):
     def test_monday_holiday(self):
         is_holiday = is_holiday_day("Monday")
         self.assertFalse(is_holiday)
+    
+    def test_sunday_holiday(self):
+        is_holiday = is_holiday_day("Sunday")
+        self.assertTrue(is_holiday)
  
 
