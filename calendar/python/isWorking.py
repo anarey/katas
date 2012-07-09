4e84b755177e463030b4ea77f8239399cbeaf447
Ana Rey <anarey@gmail.com>
2012-07-09 22:19:10 +0200

    Refector code in method is_working

--- calendar/python/isWorking.py
+++ calendar/python/isWorking.py
@@ -5,18 +5,20 @@ class IncorrectDay(Exception):
     pass
 
 def is_working_day(day):
-    if day == "":
-        raise NotDayFound
 
-    if (day != "Monday" 
-        and  day != "Tuesday" 
-        and  day != "Wednesday" 
-        and day != "Tuesday" 
-        and day != "Friday" 
-        and day != "Saturday" 
-        and day != "Sunday"):
-            raise IncorrectDay
+    is_working = ""
+    if (day == "Sunday" 
+        or day == "Saturday"):
+            is_working =  False
+    elif (day == "Monday" 
+        or day == "Tuesday" 
+        or day == "Wednesday" 
+        or day == "Tuesday" 
+        or day == "Friday"):
+            is_working = True
+    elif day == "":
+        raise NotDayFound
+    else:
+        raise IncorrectDay
     
-    if day == "Sunday" or day == "Saturday":
-        return False
-    return True
+    return is_working
