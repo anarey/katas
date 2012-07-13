074433d51a27c705ebfed8723e20efd290591ab2
Ana Rey <anarey@gmail.com>
2012-07-13 17:01:11 +0200

    Refector exception

--- parseurl_regrex/python/test_parseurl_regrex.py
+++ parseurl_regrex/python/test_parseurl_regrex.py
@@ -2,7 +2,7 @@ import unittest
 #from parseurl import get_protocol, get_site, get_path, parse_url
 from parseurl_regrex import get_protocol, get_site, get_path, parse_url
 #from parseurl import NotProtocolFound, NotSiteFound
-from parseurl_regrex import NotProtocolFound, NotSiteFound
+from parseurl_regrex import NotProtocolFoundError, NotSiteFoundError
 
 class test_parse_regrex_protocol(unittest.TestCase):
     def test_protocol_http(self):
@@ -22,7 +22,7 @@ class test_parse_regrex_protocol(unittest.TestCase):
         self.assertEqual("https", protocol)
 
     def test_not_protocol(self):
-        self.assertRaises(NotProtocolFound, get_protocol, "www.site.com")
+        self.assertRaises(NotProtocolFoundError, get_protocol, "www.site.com")
 
 class test_parse_site(unittest.TestCase):
     def test_site(self):
@@ -54,7 +54,7 @@ class test_parse_site(unittest.TestCase):
         self.assertEqual(site, "www.anarey.info")
 
     def test_not_site_found(self):
-        self.assertRaises(NotSiteFound, get_site, "http://")
+        self.assertRaises(NotSiteFoundError, get_site, "http://")
 
 class test_parse_path(unittest.TestCase):
     def test_path(self):
