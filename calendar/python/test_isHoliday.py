4edd44aa4d9ecce123363bc43a686169dd139aab
Ana Rey <anarey@gmail.com>
2012-08-10 17:39:45 +0200

    Delete calendar/python/test_isHoliday.py

--- calendar/python/test_isHoliday.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class testClassCalendar(unittest.TestCase):
-    def test_monday_holiday(self):
-        isHoliday = isHoliday_day("Monday")
-        self.assertFalse(isHoliday)
-
-       
-
